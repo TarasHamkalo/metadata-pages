1622883e3105470124cc3a6ffbef26fdb3fdb7c7
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-11-10 20:52:27 +0100

    feat: doc documents are read

--- main.py
+++ main.py
@@ -1,72 +1,14 @@
-# import glob
-# import xml.dom.minidom
-# import zipfile
-#
-#
-# def getAllFiles(allDocInfo, path):
-#   for filename in glob.iglob(path + '/**/*.*', recursive=True):
-#     allDocInfo.append(getFileInfo(filename))
-
 import logging
-import xml.dom.minidom
-import zipfile
 from importlib.metadata import metadata
-from xml.dom.minidom import Document
 
+from metadata import read_metadata_from_docx, read_metadata_from_doc, read_metadata_from_files
 
-class CoreProps:
-
-  def __init__(self, dom: Document):
-    self.creator: str = get_dom_element_as_text(dom, 'dc:creator')
-    self.lastModifiedBy: str = get_dom_element_as_text(dom, 'cp:lastModifiedBy')
-    self.DateCreated: str = get_dom_element_as_text(dom, 'dcterms:created')
-    self.DateModified: str = get_dom_element_as_text(dom, 'dcterms:modified')
-    self.lastPrinted: str = get_dom_element_as_text(dom, 'cp:lastPrinted')
-
-class AppProps:
-
-  def __init__(self, dom: Document):
-    self.template: str = get_dom_element_as_text(dom, 'Template')
-    self.totalTime: str = get_dom_element_as_text(dom, 'TotalTime')
-    self.pages: str = get_dom_element_as_text(dom, 'Pages')
-
-class Metadata:
-
-  def __init__(self, app_props: AppProps, core_props: CoreProps):
-    self.core_props: CoreProps = core_props
-    self.app_props: AppProps = app_props
-
-def read_metadata(file_path) -> Metadata:
-  with zipfile.ZipFile(file_path, 'r') as f:
-    print('Processing: ', file_path)
-    core_props: CoreProps | None = None
-
-    try:
-      core_doc = xml.dom.minidom.parseString(f.read('docProps/core.xml'))
-      core_props = CoreProps(core_doc)
-    except Exception as e:
-      logging.error('Document does not have core xml')
-
-    try:
-      app_doc = xml.dom.minidom.parseString(f.read('docProps/app.xml'))
-      app_props = AppProps(app_doc)
-    except Exception as e:
-      logging.warning('Document does not have app xml')
-
-    return Metadata(app_props, core_props)
-
-def get_dom_element_as_text(doc, tag_name) -> str:
-  try:
-    return doc.getElementsByTagName(tag_name)[0].childNodes[0].data
-  except (IndexError, AttributeError):
-    return ''
 
 def write_metadata_to_html(file_paths: list[str], output_html: str):
-  # Create an HTML file with a table
+
   with open(output_html, 'w', encoding='utf-8') as html_file:
     # Write the HTML header
     html_file.write('<html><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/><title>Metadata Report</title></head><body>\n')
-
     html_file.write('<h1>Metadata Report</h1>\n')
     html_file.write('<table border="1">\n')
 
@@ -80,20 +22,17 @@ def write_metadata_to_html(file_paths: list[str], output_html: str):
     # Process each file and append metadata rows
     for file_path in file_paths:
       try:
-        metadata = read_metadata(file_path)
-        core = metadata.core_props
-        app = metadata.app_props
-
+        metadata = read_metadata_from_docx(file_path)
         row_data = [
-          file_path,
-          core.creator if core else '',
-          core.lastModifiedBy if core else '',
-          core.DateCreated if core else '',
-          core.DateModified if core else '',
-          core.lastPrinted if core else '',
-          app.template if app else '',
-          app.totalTime if app else '',
-          app.pages if app else ''
+          metadata.path,
+          metadata.creator or '',
+          metadata.lastModifiedBy or '',
+          metadata.DateCreated or '',
+          metadata.DateModified or '',
+          metadata.lastPrinted or '',
+          metadata.template or '',
+          metadata.totalTime or '',
+          metadata.pages or ''
         ]
 
         # Write a table row with the metadata
@@ -113,6 +52,13 @@ if __name__ == "__main__":
   # savetoCSV(allDocInfo, 'results.csv')
   # getFileInfo('/home/taras-hamkalo/other/metadata-pages/19-20/processed/docx/4064284486.docx')
   # get_file_info('/home/taras-hamkalo/other/metadata-pages/19-20/processed/docx/4064284486.docx')
-  path = '../demo.docx'
-  write_metadata_to_html([path], 'report.html')
+  path = 'demo.doc'
+  # write_metadata_to_html([path], 'report.html')
+  # path = 'demo.docx'
+  # metadata = read_metadata_from_docx(path)
+  import exiftool
+  with exiftool.ExifToolHelper(common_args=['-G1', '-n']) as et:
+    metadata = read_metadata_from_doc(path, et)
+  print(metadata)
+
   print('DONE')
---
+++ metadata.py
@@ -0,0 +1,92 @@
+import logging
+import xml.dom.minidom
+import zipfile
+
+from exiftool import ExifToolHelper
+
+
+class Metadata:
+
+  def __init__(self, path):
+    self.path = path
+    self.pages: str | None
+    self.template: str | None = None
+
+    self.totalTime: int | None = None # In minutes
+
+    self.creator: str | None = None
+    self.DateCreated: str | None = None
+
+    self.lastModifiedBy: str | None = None
+    self.DateModified: str | None = None
+
+    self.lastPrinted: str | None = None
+
+def read_metadata_from_docx(file_path) -> Metadata:
+  metadata = Metadata(file_path)
+
+  with zipfile.ZipFile(file_path, 'r') as zipf:
+    logging.info('Processing %s', file_path)
+    try:
+      core = xml.dom.minidom.parseString(zipf.read('docProps/core.xml'))
+      metadata.creator = get_dom_element_as_text(core, 'dc:creator')
+      metadata.lastModifiedBy = get_dom_element_as_text(core, 'cp:lastModifiedBy')
+      metadata.DateCreated = get_dom_element_as_text(core, 'dcterms:created')
+      metadata.DateModified = get_dom_element_as_text(core, 'dcterms:modified')
+      metadata.lastPrinted = get_dom_element_as_text(core, 'cp:lastPrinted')
+    except Exception as e:
+      logging.warning('Document does not have core xml')
+
+    try:
+      app = xml.dom.minidom.parseString(zipf.read('docProps/app.xml'))
+      metadata.template = get_dom_element_as_text(app, 'Template')
+      totalTime: str= get_dom_element_as_text(app, 'TotalTime')
+      metadata.totalTime = int(totalTime) if len(totalTime) > 0 else 0
+
+      metadata.pages = get_dom_element_as_text(app, 'Pages')
+
+    except Exception as e:
+      logging.warning('Document does not have app xml')
+
+  return metadata
+
+def get_dom_element_as_text(doc, tag_name) -> str:
+  try:
+    return doc.getElementsByTagName(tag_name)[0].childNodes[0].data
+  except (IndexError, AttributeError):
+    return ''
+
+def read_metadata_from_doc(file_path: str, exif_tool: ExifToolHelper) -> Metadata:
+  metadata = Metadata(file_path)
+
+  try:
+    exif_data = exif_tool.get_metadata(file_path)[0]
+    metadata.template = exif_data.get('MS-DOC:Template') or exif_data.get('FlashPix:Template')
+    metadata.totalTime = exif_data.get('MS-DOC:TotalEditTime') or exif_data.get('FlashPix:TotalEditTime')
+    metadata.pages = exif_data.get('MS-DOC:Pages') or exif_data.get('FlashPix:Pages')
+    metadata.creator = exif_data.get('MS-DOC:Author') or exif_data.get('FlashPix:Author')
+    metadata.lastModifiedBy = exif_data.get('MS-DOC:LastModifiedBy') or exif_data.get('FlashPix:LastModifiedBy')
+    metadata.DateCreated = exif_data.get('MS-DOC:CreateDate') or exif_data.get('FlashPix:CreateDate')
+    metadata.DateModified = exif_data.get('MS-DOC:ModifyDate') or exif_data.get('FlashPix:ModifyDate')
+    metadata.lastPrinted = exif_data.get('MS-DOC:LastPrinted') or exif_data.get('FlashPix:LastPrinted')
+
+    if metadata.totalTime:
+      metadata.totalTime //= 60
+
+  except Exception as e:
+    logging.error(f"Error reading metadata for {file_path}: {e}")
+
+  return metadata
+
+def read_metadata_from_files(file_paths: list[str]) -> list[Metadata]:
+  metadata_list = []
+  # Use a single ExifTool process for all files
+  with ExifToolHelper(common_args=['-G1', '-n']) as et:
+    for file_path in file_paths:
+      if file_path.lower().endswith(('.doc', '.docx')):
+        metadata = read_metadata_from_doc(file_path, et)
+        metadata_list.append(metadata)
+      else:
+        logging.warning(f"Unsupported file type: {file_path}")
+
+  return metadata_list
